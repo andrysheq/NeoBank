dc5ea28f9b1cd4a536e2ec8c2196554b3b6a1f9f
Andrey Kareev <andrey.andreych@bk.ru>
2024-07-04 13:42:51 +0300

    some fixes

---
+++ src/main/java/NeoBankApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+//@EnableFeignClients
+//@SpringBootApplication
+//public class NeoBankApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(NeoBankApplication.class, args);
+//    }
+//}
